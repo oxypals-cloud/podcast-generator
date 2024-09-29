c2b1edcfc48652df2c4e948fa680ae46ef39818b
oxypals-cloud <mchernomaz@oxypals.com>
2024-09-29 13:52:33 +0000

    Perms

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ git config --global user.name = "${GITHUB_ACTOR}"
 git config --global user.email = "${INPUT_EMAIL}"
 git config -add safe.directory /github/workspace
 
-python3 /user/bin/feed.py
+python3 /user/local/bin/feed.py
 
 git add -A && git commit -m "Update feed"
 
